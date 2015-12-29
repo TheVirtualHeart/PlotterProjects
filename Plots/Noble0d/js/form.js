a8d91641a49987da41a55c5d46e3fb4c07dce9b6
Ben W <reccanti@gmail.com>
2015-12-29 16:16:09 -0500

    Create Form Object
    The form object has been created and is being stored in the file "form.js".
    Some basic functionality, such as form updating and setting defaults, has
    been ported over to this model.In addition, the main code has been moved
    into a file called "main.js". Over time, this code should be separated out
    into distinct modules, and eventually loaded with a module loader.

---
+++ Plots/Noble0d/js/form.js
@@ -0,0 +1,111 @@
+/**
+ * This module describes the Form element. This form can alter the appearance
+ * of the plot, describing what variables are plotted and how the Noble 
+ * differential equation is calculated.
+ */
+function NobleForm() {
+
+
+	/**
+	 * This object describes the settings for the form.
+	 */
+	var settings = {};
+	settings.defaults = {
+		v: 		   -80.0,
+		m: 			0.0,
+		h: 			1.0,
+		n: 			0.0,
+		gna1: 		400,
+		gna2: 		0.14,
+		s1: 		0,
+		ns1: 		4,
+		s2: 		2000,
+		period: 	500.0
+	};
+
+
+	/**
+	 * Reference variables for the DOM elements of the form.
+	 */
+	var controls = {
+		gna1: 			document.getElementsByName("gna1")[0],
+		gna2: 			document.getElementsByName("gna2")[0],
+		s1: 			document.getElementsByName("s1")[0],
+		ns1: 			document.getElementsByName("ns1")[0],
+		s2: 			document.getElementsByName("s2")[0],
+		period: 		document.getElementsByName("period")[0],
+		updateButton: 	document.getElementById("update"),
+		resetButton: 	document.getElementById("default"),
+		printButton: 	document.getElementById("print")
+	};
+
+
+	/**
+	 * Binds the controls on the form to their specific functions
+	 */
+	function bindUIActions() {
+		controls.resetButton.addEventListener("click", reset);
+		controls.updateButton.addEventListener("click", updatePage);
+
+		controls.gna1.addEventListener("change", updatePage);
+		controls.gna2.addEventListener("change", updatePage);
+		controls.s1.addEventListener("change", updatePage);
+		controls.ns1.addEventListener("change", updatePage);
+		controls.s2.addEventListener("change", updatePage);
+		controls.period.addEventListener("change", updatePage);
+	}
+
+
+	/**
+	 * Resets everything to a default state. This includes the form, and any
+	 * other objects on the page, such as the graph.
+	 */
+	function reset() {
+		console.log("resetting");
+		resetForm();
+		updatePage();
+	}
+
+	/**
+	 * Resets the form elements to the ones specified in settings.defaults.
+	 */
+	function resetForm() {
+		controls.gna1.value 	= settings.defaults.gna1;
+		controls.gna2.value 	= settings.defaults.gna2;
+		controls.s1.value 		= settings.defaults.s1;
+		controls.ns1.value 		= settings.defaults.ns1;                        
+		controls.s2.value 		= settings.defaults.s2;
+		controls.period.value 	= settings.defaults.period;
+	}
+
+
+	/**
+	 * Update Everything on the page. Set the values to the ones specified in 
+	 * the controls, recalculate, and call the update function.
+	 */
+	function updatePage() {
+		gna1 	= controls.gna1.value
+		gna2 	= controls.gna2.value 
+		s1 		= controls.s1.value 
+		ns1 	= controls.ns1.value
+		s2 		= controls.s2.value 
+		period 	= controls.period.value
+
+		calculate();
+		update();
+	}
+
+
+	/**
+	 * This is the object that will be returned by the function. These are the
+	 * only things that will be publicly accessible after the form is
+	 * initialized.
+	 */
+	var api = {
+		controls: controls
+	};
+
+
+	bindUIActions();
+	return api;
+}
